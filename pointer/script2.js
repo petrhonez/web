3ee091c0c0cae65528964fd62d6e16a45348646d
petrhonez <pfcandez01@gmail.com>
2024-06-14 17:13:26 -0300

    Update script2.js

--- pointer/script2.js
+++ pointer/script2.js
@@ -1,7 +1,10 @@
 document.addEventListener('mousemove', (event) => {
     const horizontalLine = document.getElementById('horizontal-line');
     const verticalLine = document.getElementById('vertical-line');
+    const customCursor = document.getElementById('custom-cursor');
 
     horizontalLine.style.top = `${event.clientY}px`;
     verticalLine.style.left = `${event.clientX}px`;
+    customCursor.style.top = `${event.clientY}px`;
+    customCursor.style.left = `${event.clientX}px`;
 });
